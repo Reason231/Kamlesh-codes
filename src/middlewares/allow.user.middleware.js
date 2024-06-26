7bfd62ebbc222d30f04316e71fe8d2bfaab188da
77-LUCCIFERZ <luccifer.says@gmail.com>
2024-06-26 21:19:03 +0545

    Update and rename allow.user.middleware.js to rbac.middleware.js

--- src/middlewares/allow.user.middleware.js
+++
@@ -1,7 +0,0 @@
-const allowUser = (req, res, next) => {
-    // person check code
-     next()                                         // it calls the next middleware
-        // next({status:401,message:"Please verify"})     // if an error occurs during allowuser it sends back to the express.config with the response of error
-  };
-
-  module.exports=allowUser
